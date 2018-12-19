27726f817c8610fad988a2ed07692138399c73af
Hanieh Shakeri <haniehs@cs.ubc.ca>
2018-12-19 00:58:58 -0800

    img size

--- autowebcompat/utils.py
+++ autowebcompat/utils.py
@@ -81,7 +81,7 @@ images = {}
 
 
 def load_image(fname, parent_dir='data_resized'):
-    img = load_img(os.path.join(parent_dir, fname), target_size=(48,48))
+    img = load_img(os.path.join(parent_dir, fname), target_size=(128,128))
     #img = load_img(os.path.join(parent_dir, fname), target_size=(224, 224))  #target_size required for pretrained weights
     return img_to_array(img, data_format=keras.backend.image_data_format())
 
