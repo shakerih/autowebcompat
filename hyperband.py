95181cbc99764f822224e0791279c233fdc7ba19
Hanieh Shakeri <haniehs@cs.ubc.ca>
2018-12-20 17:58:59 -0800

    clear session

--- hyperband.py
+++ hyperband.py
@@ -10,6 +10,7 @@ from sklearn.metrics import confusion_matrix
 
 from autowebcompat import network
 from autowebcompat import utils
+from keras import backend as K
 
 
 
@@ -82,7 +83,7 @@ def run_and_loss(num_iters, hp):
         def on_train_end(self, logs={}):
             self.train_time = time.time() - self.train_begin_time
             print("HP: network="+hp[0]+", optimizer="+str(hp[1])+", lr="+str(hp[2])+", do1="+str(hp[3])+", do2="+str(hp[4])+", l21="+str(hp[5])+", l22="+str(hp[6])+", momentum="+str(hp[7])+", nesterov="+str(hp[8])+", decay="+str(hp[9])+", epsilon="+str(hp[10]))
-
+            K.clear_session()
 
     labels = utils.read_labels('labels.csv')
 
